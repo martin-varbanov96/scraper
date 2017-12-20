4b5de2f53586f7918a8891baa0baf74486018f01
Martin Varbanov <martin_varbanov96@abv.bg>
2017-12-20 14:51:55 +0200

    spider

--- sample_project/sample_project/spiders/coverspider.py
+++ sample_project/sample_project/spiders/coverspider.py
@@ -23,3 +23,21 @@ class CoverSpider(scrapy.Spider):
         # parse it
         next = response.css("div.pages").xpath("a[contains(., 'Next')]")
         yield scrapy.Request(next.xpath("@href").extract_first(), self.parse_page)
+
+    def parse_covers(self, response):
+        # grab the URL of the cover image
+        img = response.css(".art-cover-photo figure a img").xpath("@src")
+        imageURL = img.extract_first()
+ 
+        # grab the title and publication date of the current issue
+        title = response.css(".content-main-aside h1::text").extract_first()
+        year = response.css(".content-main-aside h1 time a::text").extract_first()
+        month = response.css(".content-main-aside h1 time::text").extract_first()[:-2]
+ 
+        # parse the date
+        date = "{} {}".format(month, year).replace(".", "")
+        d = datetime.datetime.strptime(date, "%b %d %Y")
+        pub = "{}-{}-{}".format(d.year, str(d.month).zfill(2), str(d.day).zfill(2))
+ 
+        # yield the result
+        yield MagazineCover(title=title, pubDate=pub, file_urls=[imageURL])
