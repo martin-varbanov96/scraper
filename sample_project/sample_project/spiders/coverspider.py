e54b2f7ca491fd7f42e23698337d2778dc0bd207
Martin Varbanov <martin_varbanov96@abv.bg>
2017-12-20 15:00:52 +0200

    adssad

--- sample_project/sample_project/spiders/coverspider.py
+++ sample_project/sample_project/spiders/coverspider.py
@@ -14,18 +14,18 @@ class CoverSpider(scrapy.Spider):
         yield scrapy.Request(url.xpath("@href").extract_first(), self.parse_page)
 
     def parse_page(self, response):
-        # loop over all cover link elements that link off to the large 
+        # loop over all cover link elements that link off to the large
         # cover of the magazine and yield a request to grab the cove
         # data and image
         for href in response.xpath("//a[contains(., 'Large Cover')]"):
-        yield scrapy.Request(href.xpath("@href").extract_first(),
-        self.parse_covers)
-
+            yield scrapy.Request(href.xpath("@href").extract_first(),
+                self.parse_covers)
+ 
         # extract the 'Next' link from the pagination, load it, and
         # parse it
         next = response.css("div.pages").xpath("a[contains(., 'Next')]")
         yield scrapy.Request(next.xpath("@href").extract_first(), self.parse_page)
-
+        
     def parse_covers(self, response):
         # grab the URL of the cover image
         img = response.css(".art-cover-photo figure a img").xpath("@src")
