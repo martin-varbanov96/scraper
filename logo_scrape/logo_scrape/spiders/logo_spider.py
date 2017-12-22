4654d179b635980107e8d732b7e69e594b465380
Martin Varbanov <martin_varbanov96@abv.bg>
2017-12-22 13:05:06 +0200

    adas

--- logo_scrape/logo_scrape/spiders/logo_spider.py
+++ logo_scrape/logo_scrape/spiders/logo_spider.py
@@ -6,29 +6,30 @@ import scrapy
 
 class LogoSpider(scrapy.Spider):
     name = "logo_spider"
-    start_urls = ["http://time.com"]
+    # start_urls = ["http://time.com"]
+
+    def start_requests(self):
+        yield scrapy.Request("http://time.com", self.parse)
 
     def parse(self, response):
-        def harcode_img():
-
-            #Here we try to find the logo in pattern:
-            #<div id="header"><a><img>
-            headers = ["#Header", "#header", "#HEADER"]
-            for header in headers:
-                pattern_header=response.css(header)
-                if pattern_header:
-                    if pattern_header.xpath("//a/img/@src"):
-                        url = response
-                        file_url = pattern_header.xpath("//a/img/@src").extract_first()
-                        yield LogoScrapeItem(title=url, file_urls=file_url)
-
-            # Here we try to find logo if site uses <header>
-
-            if response.xpath("//header"):
-                #<header><a><img>
-                header_a_img = response.xpath("//header//a/img/@src")
-                if header_a_img:
+        #Here we try to find the logo in pattern:
+        #<div id="header"><a><img>
+        headers = ["#Header", "#header", "#HEADER"]
+        for header in headers:
+            pattern_header=response.css(header)
+            if pattern_header:
+                if pattern_header.xpath("//a/img/@src"):
                     url = response
-                    file_url = header_a_img.extract_first()
+                    file_url = pattern_header.xpath("//a/img/@src").extract_first()
+                    yield LogoScrapeItem(title=url, file_urls=file_url)
 
-        harcode_img()
+        # Here we try to find logo if site uses <header>
+
+        if response.xpath("//header"):
+            #<header><a><img>
+            header_a_img = response.xpath("//header//a/img/@src")
+            if header_a_img:
+                url = response
+                file_url = header_a_img.extract_first()
+
+        # harcode_img()
