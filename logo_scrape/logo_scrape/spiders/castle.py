f52a21bcb80a61d3ad150ed16965d443e418c100
Martin Avramov <martin_varbanov96@abv.bg>
2017-12-25 10:57:35 +0000

    class-not-working

--- logo_scrape/logo_scrape/spiders/castle.py
+++
@@ -1,5 +0,0 @@
-for i in range(0, 5):
-	a = 5
-	a = 2 if i %2 == 0 else a = 5
-	print(a)
-	a=3
--- logo_scrape/logo_scrape/spiders/logo_spider.py
+++ logo_scrape/logo_scrape/spiders/logo_spider.py
@@ -3,6 +3,25 @@ from logo_scrape.items import LogoScrapeItem
 import datetime
 import scrapy
 
+class PatternUrl:
+
+    def __init__(self, path_to_img="", list_of_conditionals=[]):
+        self.url_pattern = ""
+        self.file_url = ""
+        self.path_to_img = path_to_img
+        self.list_of_conditionals = list_of_conditionals
+
+    def find_obj(self, response):
+        print("asdds"*50)
+        for el in self.list_of_conditionals:
+            if el:
+                if self.path_to_img:
+                    print("500"*500)
+                    url = response
+                    file_url = str(self.path_to_img)
+                    print(url)
+                    print(file_url)
+                    yield LogoScrapeItem(url=url, file_url=file_url)
 
 class LogoSpider(scrapy.Spider):
     name = "logo_spider"
@@ -11,11 +30,8 @@ class LogoSpider(scrapy.Spider):
     def start_requests(self):
         yield scrapy.Request("http://time.com", self.parse)
         yield scrapy.Request("https://git-scm.com/docs/git-merge", self.parse)
-        yield scrapy.Request("https://www.pythoncentral.io/one-line-if-statement-in-python-ternary-conditional-operator/", self.parse)
 
     def parse(self, response):
-        # TODO:: Here these methods make may copy burger button or similar
-
         patterns_trees = [response.css("#Header").xpath("//a/img/@src"), 
                             response.css("#HEADER").xpath("//a/img/@src"),    
                             response.css("#header").xpath("//a/img/@src"),
@@ -28,5 +44,10 @@ class LogoSpider(scrapy.Spider):
             #<header><a><img>
                 url = response
                 file_url = pattern_tree.extract_first()
-                yield LogoScrapeItem(title=url, file_urls=file_url)
-        
+                yield LogoScrapeItem(url=url, file_url=file_url)
+        home_page_url_patter = "" 
+#        a = PatternUrl(response.css("header").xpath("//a[@href='"+response.url+'/'+"']/img/@src").extract_first(), [response.css("header").xpath("//a[@href='"+response.url+'/'+"']")] )
+ #       a.find_obj(response)
+        # response.css("header").xpath("//a[contains(@href, 'http://time.com/')]").re(r'"http://time.com/"')
+
+
