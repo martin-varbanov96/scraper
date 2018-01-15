6362b03d93552cf389659a9019702c17ea69c07d
Martin Varbanov <martin_varbanov96@abv.bg>
2018-01-15 18:13:00 +0100

    adassad

--- logo_scrape/logo_scrape/spiders/logo_spider.py
+++ logo_scrape/logo_scrape/spiders/logo_spider.py
@@ -3,6 +3,7 @@ import datetime
 import scrapy
 import csv
 import re
+from collections import Counter
 import logging
 
 def get_data():
@@ -18,25 +19,8 @@ def get_data():
                 # output_list.append(el_https)
         return output_list
 
-class PatternUrl:
-
-    def __init__(self, path_to_img="", list_of_conditionals=[]):
-        self.url_pattern = ""
-        self.file_url = ""
-        self.path_to_img = path_to_img
-        self.list_of_conditionals = list_of_conditionals
-
-    def find_obj(self, response):
-        for el in self.list_of_conditionals:
-            if el:
-                if self.path_to_img:
-                    url = response
-                    file_url = str(self.path_to_img)
-                    yield LogoScrapeItem(url=url, file_url=file_url)
-
 class LogoSpider(scrapy.Spider):
     name = "logo_spider"
-    #start_urls = ["http://time.com"]
 
     def start_requests(self):
        # for el in get_data():
@@ -48,57 +32,66 @@ class LogoSpider(scrapy.Spider):
        # except TimeoutError as e:
         #    logging.exception("MSG"*500)
 
-            
     def parse(self, response):
         pattern_regex = r"<[0-9][0-9][0-9]\W"
-        response_status_code=int(re.match(pattern_regex, str(response)).group(0)[1:4])
+        response_status_code = int(re.match(pattern_regex,
+                                   str(response)).group(0)[1:4])
         if response_status_code != 200:
             yield LogoScrapeItem(reponse_code=response_status_code)
-
-        #Here we iterate over 10 random links
-        iterated_links_counter=0
-        for href in response.xpath("//a"):
-            iterated_links_counter +=1
-            if iterated_links_counter >= 10:
-                break
-            #TODO:: Implement second parse
-            #yield scrapy.Requst(href.xpath("@href").extract_first(), self.PARSE_FUNCTION_IMPLEMENT)
-            pass
-
-
         # TODO:: We add false data
 
-        #here we set possible patterns and check if they are found:
+        # here we set possible patterns and check if they are found:
         patterns_trees = [response.css("#Header").xpath("//a/img/@src"), 
-                            response.css("#HEADER").xpath("//a/img/@src"),    
-                            response.css("#header").xpath("//a/img/@src"),
-                            response.xpath("//header//a/img/@src"),
-                            response.xpath("//a[@href='"+response.url+'/'+"']/img/@src"),
-                            response.xpath("//a[@href='"+response.url+'/'+"']/img/@src"),
-                            response.css("header").xpath("//a[@href='/']/img/@src"),
-                            response.xpath("//a[@href='/']/img/@src")
-        ]
-        # TODO:: check status codes
-        #if response.getcode() != 200:
-         #       url = response
-          #      response_code=response.getcode()
-           #     yield LogoScrapeItem(url=url, response_code=response_code)
-        pattern_number = 0 
+                          response.css("#HEADER").xpath("//a/img/@src"),    
+                          response.css("#header").xpath("//a/img/@src"),
+                          response.xpath("//header//a/img/@src"),
+                          response.xpath("//a[@href='"+response.url+'/'+"']/img/@src"),
+                          response.xpath("//a[@href='"+response.url+'/'+"']/img/@src"),
+                          response.css("header").xpath("//a[@href='/']/img/@src"),
+                          response.xpath("//a[@href='/']/img/@src")
+                          ]
+        pattern_number = 0
         for pattern_tree in patterns_trees:
-            if pattern_tree:
+            def parse_within_page(self, response):
+                return pattern_tree
+
+            def getFinalImage(input_dict):
+                greatest_count = 0
+                greatest_string = ""
+                for el in input_dict:
+                    if greatest_count < input_dict[el]:
+                        greatest_count = input_dict[el]
+                        greatest_string = el
+                return greatest_string, greatest_count
+            # Here we iterate over 10 random links
+            iterated_links_counter = 0
+            links_list = list()
+            flag_to_yield = False
+            most_common_image_found = ""
+            amount_most_common_image_found = 0
+            for href in response.xpath("//a"):
+                    iterated_links_counter += 1
+                    if iterated_links_counter >= 10:
+                        break
+                    pattern_response = scrapy.Request(href.xpath("@href").extract_first(),
+                                                      self.parse_within_page())
+                    if pattern_response:
+                        flag_to_yield = True
+                        links_list.append(pattern_response)
+            links_dict = Counter(links_list)
+            most_common_image_found, amount_most_common_image_found = getFinalImage(links_dict)
+
+            if flag_to_yield:
                 url = response
-
-                # TODO:: RETURN STATUS CODE
-                #response_code=response.getcode()
-                
                 pattern_type = pattern_number
-                pattern_number+=1
+                pattern_number += 1
                 file_url = pattern_tree.extract_first()
-                # TODO:: Add response code
-                yield LogoScrapeItem(url=url, file_url=file_url,pattern_type=pattern_type, response_code=response_status_code, found_img=True, is_http="", is_https="")
-        home_page_url_patter = "" 
-#        a = PatternUrl(response.css("header").xpath("//a[@href='"+response.url+'/'+"']/img/@src").extract_first(), [response.css("header").xpath("//a[@href='"+response.url+'/'+"']")] )
- #       a.find_obj(response)
-        # response.css("header").xpath("//a[contains(@href, 'http://time.com/')]").re(r'"http://time.com/"')
-
+                yield LogoScrapeItem(url=url,
+                                     file_url=file_url,
+                                     pattern_type=pattern_type,
+                                     response_code=response_status_code,
+                                     found_img=True,
+                                     is_http="",
+                                     is_https="",
+                                     succsses_rate=amount_of_same_image)
 
